a2f1e3c083007870c393fbf7e643cd757446fa25
kubo <jakub.jursa@relbit.com>
2013-04-23 02:26:13 +0200

    hardware type removed

--- app/models/hardware_type.rb
+++
@@ -1,9 +0,0 @@
-class HardwareType < ActiveRecord::Base
-  attr_accessible :name
-
-  validates :name,
-            presence: true,
-            length:   { maximum: 50 },
-            uniqueness: { case_sensitive: false }
-  before_save { |hwtype| hwtype.name.downcase! }
-end
